6536c8f5a3e1c70526a8a216378b725eab0c5cf0
futtta <futtta@gmail.com>
2017-12-07 09:15:48 +0100

    and finally; google fonts optimization

--- ao_extra.php
+++ ao_extra.php
@@ -8,13 +8,47 @@ Version: 0.2.0
 Author URI: http://optimizingmatters.com/ 
 */
 
+// get option-array
 $ao_extra_options = get_option( 'ao_extra_settings' );
 
-/* disable emojis */
-if ($ao_extra_options['ao_extra_checkbox_field_1']) {
-    add_action( 'init', 'ao_extra_disable_emojis' );
+// initialize the extra's
+add_action('init','ao_extra_init');
+function ao_extra_init() {
+    global $ao_extra_options;
+    
+    /* disable emojis */
+    if ($ao_extra_options['ao_extra_checkbox_field_1']) {
+       ao_extra_disable_emojis();
+    }
+    
+    /* remove version from query string */
+    if ($ao_extra_options['ao_extra_checkbox_field_0']) {
+        add_filter( 'script_loader_src', 'ao_extra_remove_qs', 15, 1 );
+        add_filter( 'style_loader_src', 'ao_extra_remove_qs', 15, 1 );
+    }
+
+    /* async JS */
+    if (!empty($ao_extra_options['ao_extra_text_field_3'])) {
+        add_filter('autoptimize_filter_js_exclude','ao_extra_async_js',10,1);
+    }
+
+    /* optimize google fonts */
+    if ( !empty( $ao_extra_options['ao_extra_radio_field_4'] ) && ( $ao_extra_options['ao_extra_radio_field_4'] != "1" ) ) {
+        if ( $ao_extra_options['ao_extra_radio_field_4'] == "2" ) {
+            add_filter('autoptimize_filter_css_removables','ao_extra_remove_gfonts',10,1);
+        } else {
+            add_filter('autoptimize_html_after_minify','ao_extra_gfonts',10,1);
+            add_filter('ao_extra_filter_tobepreconn','ao_extra_preconnectgooglefonts',10,1);
+        }
+    }
+    
+    /* preconnect */
+    if ( !empty($ao_extra_options['ao_extra_text_field_2']) || has_filter('ao_extra_filter_tobepreconn') ) {
+        add_filter( 'wp_resource_hints', 'ao_extra_preconnect', 10, 2 );
+    }
 }
 
+// disable emoji's functions
 function ao_extra_disable_emojis() {
   // all actions related to emojis
   remove_action( 'admin_print_styles', 'print_emoji_styles' );
@@ -37,12 +71,7 @@ function ao_extra_disable_emojis_tinymce( $plugins ) {
   }
 }
 
-/* remove version from query string */
-if ($ao_extra_options['ao_extra_checkbox_field_0']) {
-    add_filter( 'script_loader_src', 'ao_extra_remove_qs', 15, 1 );
-    add_filter( 'style_loader_src', 'ao_extra_remove_qs', 15, 1 );
-}
-
+// remove query string function
 function ao_extra_remove_qs( $src ) {
         if ( strpos($src, '?ver=') ) {
                 $src = remove_query_arg( 'ver', $src );
@@ -50,11 +79,7 @@ function ao_extra_remove_qs( $src ) {
         return $src;
 }
 
-/* async JS */
-if (!empty($ao_extra_options['ao_extra_text_field_3'])) {
-    add_filter('autoptimize_filter_js_exclude','ao_extra_async_js',11,1);
-}
-
+// async function
 function ao_extra_async_js($in) {
     global $ao_extra_options;
     
@@ -76,16 +101,13 @@ function ao_extra_async_js($in) {
     return $AO_excl_w_async;
 }
 
-/* preconnect */
-if (!empty($ao_extra_options['ao_extra_text_field_2'])) {
-    add_filter( 'wp_resource_hints', 'ao_extra_preconnect', 10, 2 );
-}
+// preconnect function
 function ao_extra_preconnect($hints, $relation_type) {
     global $ao_extra_options;
 
     // get setting and store in array
     $_to_be_preconnected = array_filter(array_map('trim',explode(",",$ao_extra_options['ao_extra_text_field_2'])));
-    $_to_be_preconnected = apply_filters( 'ao_extra_tobepreconnected', $_to_be_preconnected );
+    $_to_be_preconnected = apply_filters( 'ao_extra_filter_tobepreconn', $_to_be_preconnected );
 
     // walk array, extract domain and add to new array with crossorigin attribute
     foreach ($_to_be_preconnected as $_preconn_single) {
@@ -109,6 +131,99 @@ function ao_extra_preconnect($hints, $relation_type) {
     
     return $hints;
 }
+
+// google font functions
+function ao_extra_remove_gfonts($in) { 
+    // simply remove google fonts
+    return $in.", fonts.googleapis.com"; 
+}
+
+function ao_extra_gfonts($in) {
+    global $ao_extra_options;
+    
+	// extract fonts
+	$_without_comments = preg_replace( '/<!--(.*)-->/Uis', '', $in );
+    preg_match_all( '#<link(?:\s+(?:(?!href\s*=\s*)[^>])+)?(?:\s+href\s*=\s*([\'"])((?:https?:)?\/\/fonts\.googleapis\.com\/css(?:(?!\1).)+)\1)(?:\s+[^>]*)?>#iU', $_without_comments, $matches );
+
+	$i = 0;
+	$fontsCollection = array();
+	if ( ! $matches[2] ) {
+		return $in;
+	}
+    
+    // store them in $fonts array
+	foreach ( $matches[2] as $font ) {
+		if ( ! preg_match( '/rel=["\']dns-prefetch["\']/', $matches[0][ $i ] ) ) {
+			// Get fonts name.
+			// $font = str_replace( array( '%7C', '%7c' ) , '|', $font );
+            $font = urldecode($font);
+			$font = explode( 'family=', $font );
+			$font = ( isset( $font[1] ) ) ? explode( '&', $font[1] ) : array();
+			// Add font to $fonts[$i]
+		    $fontsCollection[$i]["fonts"] = explode( '|', reset( $font ) );
+		    // And add subset if any
+			$subset = ( is_array( $font ) ) ? end( $font ) : '';
+		    if ( false !== strpos( $subset, 'subset=' ) ) {
+				$subset = explode( 'subset=', $subset );
+				$fontsCollection[$i]["subsets"] = explode( ',', $subset[1] );
+		    }
+		    // And remove Google Fonts.
+		    $in = str_replace( $matches[0][ $i ], '', $in );
+		}
+	    $i++;
+	}
+
+    if ( $ao_extra_options['ao_extra_radio_field_4'] == "3" ) {
+        // as link
+        $_fontsString="";
+        foreach ($fontsCollection as $font) {
+            $_fontsString .= trim( implode( '|' , $font["fonts"] ), '|' );
+            if ( !empty( $font["subsets"] ) ) {
+                $subsetString .= implode( ',', $font["subsets"] ); 
+            }
+        }
+        
+        if (!empty($subsetString)) {
+            $_fontsString = $_fontsString."#038;subset=".$subsetString;
+        }
+        
+        if ( ! empty( $_fontsString ) ) {
+            $_fontsOut = '<link rel="stylesheet" id="ao_optimized_gfonts" href="https://fonts.googleapis.com/css?family=' . $_fontsString . '" />';
+        }
+    } else if ( $ao_extra_options['ao_extra_radio_field_4'] == "4" ) {
+        // webfont.js impl.
+        $_fontsArray = array();
+        foreach ($fontsCollection as $_fonts) {
+            if ( !empty( $_fonts["subsets"] ) ) {
+                $_subset = implode(",",$_fonts["subsets"]);
+                foreach ($_fonts["fonts"] as $key => $_one_font) {
+                    $_one_font = $_one_font.":".$_subset;
+                    $_fonts["fonts"][$key] = $_one_font;
+                } 
+            }
+            $_fontsArray = array_merge($_fontsArray, $_fonts["fonts"]);
+        }
+        
+        $_fontsOut = '<script data-cfasync="false" type="text/javascript">WebFontConfig={google:{families:[\'';
+        foreach ($_fontsArray as $_font) {
+            $_fontsOut .= $_font."','";
+        }
+        $_fontsOut = trim(trim($_fontsOut,"'"),",");
+        $_fontsOut .= '] },classes:false, events:false, timeout:1500};(function() {var wf = document.createElement(\'script\');wf.src=\'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js\';wf.type=\'text/javascript\';wf.async=\'true\';var s=document.getElementsByTagName(\'script\')[0];s.parentNode.insertBefore(wf, s);})();</script>';
+    }
+ 
+    // inject in HTML
+    $out = substr_replace($in, $_fontsOut."<link", strpos($in, "<link"), strlen("<link"));
+	return $out;
+}
+
+function ao_extra_preconnectgooglefonts($in) {
+    global $ao_extra_options;
+    // preconnect to fonts.googleapis.com speed up download of static font-files
+    $in[] = "https://fonts.googleapis.com";
+    return $in;
+}
+
 
 /* admin page */
 if ( is_admin() ) {
@@ -154,6 +269,13 @@ function ao_extra_settings_init(  ) {
 		'ao_extra_text_field_3', 
 		__( 'Async Javascript-files', 'autoptimize' ), 
 		'ao_extra_text_field_3_render', 
+		'ao_extra_settings', 
+		'ao_extra_pluginPage_section'
+    );
+    add_settings_field( 
+		'ao_extra_radio_field_4', 
+		__( 'Google Fonts', 'autoptimize' ), 
+		'ao_extra_radio_field_4_render', 
 		'ao_extra_settings', 
 		'ao_extra_pluginPage_section'
     );
@@ -206,6 +328,17 @@ function ao_extra_text_field_3_render() {
 	<input type='text' style='width:80%' name='ao_extra_settings[ao_extra_text_field_3]' value='<?php echo $ao_extra_options['ao_extra_text_field_3']; ?>'><br />
 	<?php
     _e('Comma-separated list of local or 3rd party JS-files that should loaded with the <code>async</code> flag. JS-files from your own site will be automatically excluded if added here.','autoptimize');
+}
+
+function ao_extra_radio_field_4_render() { 
+   	global $ao_extra_options;
+    $_googlef = $ao_extra_options['ao_extra_radio_field_4'];
+	?>
+    <input type="radio" name="ao_extra_settings[ao_extra_radio_field_4]" value="1" <?php if (!in_array($_googlef,array(2,3,4))) {echo "checked"; }  ?>><?php _e('Leave as is','autoptimize')?><br/>
+    <input type="radio" name="ao_extra_settings[ao_extra_radio_field_4]" value="2" <?php checked(2, $_googlef, true); ?>><?php _e('Remove Google Fonts','autoptimize')?><br/>
+    <input type="radio" name="ao_extra_settings[ao_extra_radio_field_4]" value="3" <?php checked(3, $_googlef, true); ?>><?php _e('Combine and link in head','autoptimize')?><br/>
+    <input type="radio" name="ao_extra_settings[ao_extra_radio_field_4]" value="4" <?php checked(4, $_googlef, true); ?>><?php _e('Combina and load async with webfont.js','autoptimize')?><br/>
+    <?php
 }
 
 function ao_extra_settings_section_callback() {
